ddbb3df2ef50272b07f0e356b1fb7f0d1b8b4256
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-21 21:54:38 +0530

    [Prajakta]: Added: Used Lambok Library for Logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,19 @@
 package com.bridgelabz.employeepayrollapp;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		log.info("Employee Payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
+		
 	}
 
 }
