c2050dc53f882a9e6d6f92d6d5e1595ed8b160a8
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-22 12:14:18 +0530

    [Prajakta]: Added: Validation is done on the Payroll DTO and Exception Handler For Incorrect Date Format

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.ToString;
@@ -18,15 +21,21 @@ public @ToString class EmployeePayrollDTO
 	@Min(value = 500, message = "Min wage should be more than 500")
 	public long salary;
 	
+	@Pattern(regexp = "male|female", message = "Gender needs to be male or female")
 	public String gender;
 	
 	@JsonFormat(pattern = "dd MMM yyyy")
+	@NotNull(message = "Start date should not be empty")
+	@PastOrPresent(message = "start date should be past or present date")
 	public LocalDate startDate;
 	
+	@NotBlank(message = "Note cannot be blank")
 	public String note;
 	
+	@NotBlank(message = "ProfilePic cannot be blank")
 	public String profilePic;
 	
+	@NotNull(message = "Department cannot be empty")
 	public List<String> departments;
 		
 }
