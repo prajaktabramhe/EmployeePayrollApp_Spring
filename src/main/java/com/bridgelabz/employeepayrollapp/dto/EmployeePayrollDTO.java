c5fbd0e91b7f6d87e157aace956f9ff865ffeb89
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-22 11:15:04 +0530

    [Prajakta]: Added: Refactore: Code for Environment Variable

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -3,7 +3,9 @@ package com.bridgelabz.employeepayrollapp.dto;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
-public class EmployeePayrollDTO 
+import lombok.Data;
+
+public @Data class EmployeePayrollDTO 
 {
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
